f2a164c7e3064602bd440e4c7862d9c8749fb664
Fares Fraij <fares@utexas.edu>
2020-07-04 21:24:39 -0500

    uploading additional material

---
+++ exercises/Cycle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from itertools import cycle
+
+# ------
+# Cycle.py
+# ------
+
+# cycle(p): iterates through a given iterable indefinitely until explicitly broken
+def cycle_range_for(s) :
+   if s :
+      while True :
+         for i in range(len(s)) :
+            yield s[i]
+   else :
+      pass
+   
+def cycle_for (s) : # user-defined iterable
+    p = iter(s)
+    try :
+        while True :
+           w = next(p)
+           yield w
+    except StopIteration :
+        pass
+    
