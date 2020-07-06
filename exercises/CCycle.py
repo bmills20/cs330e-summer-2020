fc474fb964200e4d1337f9c8569e2092949e1f83
Fares Fraij <fares@utexas.edu>
2020-07-06 14:05:19 -0500

    uploading additional materail

--- exercises/CCycle.py
+++ exercises/CCycle.py
@@ -31,27 +31,3 @@ class cycle_class() :
                self.s = iter(self.t)
                self.next_item = next(self.s)
            return i
-
-
-
-
-'''
-class cycle_class() :
-   def __init__(self, s) :
-      self.s = s
-      self.i = 0
-      self.size = len(s)
-   
-   def __iter__(self) :
-      return self
-      
-   def __next__(self) :
-      if self.size == 0 :
-         raise StopIteration() 
-      else :          
-         j = self.i
-         self.i += 1
-         if self.i == self.size : 
-            self.i = 0
-         return self.s[j]
- '''
